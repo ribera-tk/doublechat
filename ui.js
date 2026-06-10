c5982afc389346763419939e4ac826bdad3dbb66
ribera-tk <ribera.tk@gmail.com>
2026-06-10 16:29:46 +0900

    Update ui.js

--- ui.js
+++ ui.js
@@ -146,7 +146,8 @@
       if (!text || input.disabled) return;
       
       // カスタムイベントを発火してCoreに処理を委譲
-      document.dispatchEvent(new CustomEvent('dc-request-send', { detail: text }));
+      document.dispatchEvent(new CustomEvent('dc-request-send', { 
+  detail: { text: text }
       
       input.value = "";
       adjustInputHeight();
